01a97afbfe5183ece5de7a93b70c8b69c885da98
TheRealDemmy <Dendimir101@yahoo.com>
2024-03-08 22:49:02 +0100

    Shooting Phase 1 Finished

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -6,10 +6,19 @@ public class Bullet : MonoBehaviour
 {
     //public GameObject hitEffect;
 
+    private void Update()
+    {
+        Destroy(gameObject, 5f);
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         //GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
         //Destroy(effect, 3f);
-        Destroy(gameObject);
+        if (collision.gameObject.tag == "enemy")
+        {
+            Destroy(gameObject);
+        }
+            
     }
 }
--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -12,16 +12,50 @@ public class Shooting : MonoBehaviour
 
     public float bulletForce = 20f;
 
+    public float RateOfFire = 0.3f;
+    public float ElapsedTime = 0f;
+
+    public int ShootStyle = 1;
+
+    public Transform spread1;
+    public Transform spread2;
+
+
     Vector2 mousePos;
 
     // Update is called once per frame
     void Update()
     {
         mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        ElapsedTime += Time.deltaTime;
 
-        if (Input.GetButtonDown("Fire1"))
+        switch (ShootStyle)
         {
-            Shoot();
+            case 1:
+                RateOfFire = 0.4f;
+                if (Input.GetButton("Fire1") && ElapsedTime >= RateOfFire)
+                {
+                    Shoot();
+                    ElapsedTime = 0;
+                }
+                break;
+            case 2:
+                RateOfFire = 0.6f;
+                if (Input.GetButton("Fire1") && ElapsedTime >= RateOfFire)
+                {
+                    SpreadShoot();
+                    ElapsedTime = 0;
+                }
+                break;
+            case 3:
+                RateOfFire = 0.8f;
+                if (Input.GetButton("Fire1") && ElapsedTime >= RateOfFire)
+                {
+                    StartCoroutine(BurstShoot());
+                    ElapsedTime = 0;
+                }
+                break;
+           
         }
     }
 
@@ -37,5 +71,34 @@ public class Shooting : MonoBehaviour
         GameObject bullet =  Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
         Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
         rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+    }
+    
+    public void SpreadShoot()
+    {
+        GameObject bullet =  Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+
+        GameObject bullet2 = Instantiate(bulletPrefab, spread1.position, spread1.rotation);
+        Rigidbody2D rb2 = bullet2.GetComponent<Rigidbody2D>();
+        rb2.AddForce(spread1.up * bulletForce, ForceMode2D.Impulse);
+
+        GameObject bullet3 = Instantiate(bulletPrefab, spread2.position, spread2.rotation);
+        Rigidbody2D rb3 = bullet3.GetComponent<Rigidbody2D>();
+        rb3.AddForce(spread2.up * bulletForce, ForceMode2D.Impulse);
+    }
+    
+    IEnumerator BurstShoot()
+    {
+        int BulletCount = 3;
+        float BurstRate = 0.1f;
+
+        for (int i = 0; i < BulletCount; i++)
+        {
+            GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+            Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+            rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+            yield return new WaitForSeconds(BurstRate);
+        }
     }
 }
