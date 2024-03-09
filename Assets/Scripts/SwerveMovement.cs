74e28b714b7591ebefcf8efd472a02bbad256436
Rigamorphius <43024627+Rigamorphius@users.noreply.github.com>
2024-03-09 20:07:40 +0100

    Added notes to Code

--- Assets/Scripts/SwerveMovement.cs
+++ Assets/Scripts/SwerveMovement.cs
@@ -38,45 +38,53 @@ public class SwerveMovement : MonoBehaviour
 
     void Applyingforce()
     {
-
+        //Calculates how much forward we are going
         VelocityVSup = Vector2.Dot(transform.up, rb.velocity);
 
+        //Limit so you cant go faster than the mac speed
         if (VelocityVSup > maxspeed && accelerationinput > 0) {
             return;
         }
 
+        //Limit so we cant go faster than 50% max speed backwards
         if (VelocityVSup < maxspeed * 0.05f && accelerationinput < 0)
         {
             return;
         }
 
+        //Limit so we cant go faster in any direction accelerating
         if (rb.velocity.sqrMagnitude > maxspeed * maxspeed && accelerationinput > 0)
         {
             return;
         }
 
+        //Apply drag if there is no acceleration and stops when letting go of the button
         if (accelerationinput == 0) {
             rb.drag = Mathf.Lerp(rb.drag, 3.0f, Time.deltaTime * 3);
     }   else rb.drag = 0;
 
-
+        //Creates a force for the Ball
         Vector2 forward = transform.up * accelerationinput * accelerationfactor;
 
+        //Apply force and moves the ball forward
         rb.AddForce(forward, ForceMode2D.Force);
     }
 
     void ApplySteering()
     {
+        //Limit ball ability to turn when moving slowly
         float minspeed = (rb.velocity.magnitude / 4);
         minspeed = Mathf.Clamp01(minspeed);
         
-        
+        //Update rotation angle based on input
         rotationangle -= steeringinput * turnFactor * minspeed;
 
+        //Apply steering by rotating the ball
         rb.MoveRotation(rotationangle);
     }
 
 
+    //Car physics stuff
     void KillOrthogonalVelocity() {
         Vector2 forwardVelocity = transform.up * Vector2.Dot(rb.velocity, transform.up);
         Vector2 rightVelocity = transform.right * Vector2.Dot(rb.velocity, transform.right);
