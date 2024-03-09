e97baba315bbcdd6f8edd638ff9b97aebfa14398
TheRealDemmy <Dendimir101@yahoo.com>
2024-03-09 17:52:48 +0100

    ExplosionShot Completo

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -6,6 +6,19 @@ public class Bullet : MonoBehaviour
 {
     //public GameObject hitEffect;
 
+    public GameObject shooter;
+    public Shooting shootStyle;
+
+    public GameObject bulletFab;
+
+    private float angle;
+
+    private void Start()
+    {
+        shooter = GameObject.FindWithTag("Shooter");
+        shootStyle = shooter.GetComponent<Shooting>();
+    }
+
     private void Update()
     {
         Destroy(gameObject, 3f);
@@ -15,10 +28,23 @@ public class Bullet : MonoBehaviour
     {
         //GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
         //Destroy(effect, 3f);
-        if (collision.gameObject.tag == "enemy")
+        if (collision.gameObject.tag == "enemy" && shootStyle.ShootStyle != 4)
         {
             Destroy(gameObject);
         }
-            
+        
+        if (shootStyle.ShootStyle == 4 && collision.gameObject.tag == "enemy")
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Quaternion target = Quaternion.Euler(0, 0, angle);
+                GameObject bullet = Instantiate(bulletFab, collision.transform.position, target);
+                Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+                rb.AddForce(bullet.transform.up * shootStyle.bulletForce, ForceMode2D.Impulse);
+                angle = angle + 72;
+            }
+            Destroy(gameObject);
+            //hehe
+        }
     }
 }
